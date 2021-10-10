78da11c1c0484f29b787b7d9a819380213044ef6
cothan <cothannguyen@gmail.com>
2021-10-10 00:28:16 -0400

    correct level 5,4

--- neon-512/autobuild.sh
+++ neon-512/autobuild.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# set -e 
-# gcc -o neon-fft fips202.c fpr.c  inner.c util.c fft.c neon_fft.c -O3 -Wall
-# ./neon-fft
+set -e 
+gcc -o neon-fft fips202.c fpr.c  inner.c util.c fft.c neon_fft.c -O0 -g3 -Wall
+./neon-fft
+./neon-fft | md5sum - 
 
 # set -e 
-while inotifywait -e close_write fft.c; do 
-    gcc -o neon-fft fips202.c fpr.c  inner.c util.c fft.c neon_fft.c -O3 -g3 -Wall
-    # ./neon-fft | md5sum - 
-    ./neon-fft
-done
+# while inotifywait -e close_write fft.c; do 
+#     gcc -o neon-fft fips202.c fpr.c  inner.c util.c fft.c neon_fft.c -O0 -g3 -Wall
+#     ./neon-fft
+#     ./neon-fft | md5sum - 
+# done
