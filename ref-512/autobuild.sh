6bcdbc32d0397814f2a39244833c08152f022f0b
cothan <cothannguyen@gmail.com>
2021-10-10 05:51:17 -0400

    update

--- ref-512/autobuild.sh
+++ ref-512/autobuild.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# set -e 
+# gcc -o ref-fft util.c fpr.c fft.c ref-fft.c -O0 -g3; 
+# ./ref-fft | md5sum - 
+
 set -e 
-gcc -o ref-fft util.c fpr.c fft.c ref-fft.c -O0 -g3; 
-./ref-fft
+while inotifywait -e close_write fft.c; do 
+    gcc -o ref-fft util.c fpr.c fft.c ref-fft.c -O0 -g3
+    ./ref-fft
+    ./ref-fft | md5sum -
+done
