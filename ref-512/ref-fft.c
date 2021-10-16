2fec8a49cf095f5d12bd9aef1099e180066b0398
cothan <cothannguyen@gmail.com>
2021-10-16 03:09:42 -0400

    update for test

--- ref-512/ref-fft.c
+++ ref-512/ref-fft.c
@@ -8,7 +8,10 @@
 int main()
 {
     fpr f_gold[FALCON_N], f_test[FALCON_N];
+    fpr f0_gold[FALCON_N], f1_gold[FALCON_N];
+    fpr f0_test[FALCON_N], f1_test[FALCON_N];
     fpr tmp;
+    int size = 3;
     int ret = 0;
 
     for (int i = 0; i < FALCON_N; i++)
@@ -19,15 +22,22 @@ int main()
     }
     // print_array(f_gold, FALCON_N, "Before FFT", 1);
     // PQCLEAN_FALCON512_CLEAN_iFFT(f_gold, FALCON_LOGN);
-    PQCLEAN_FALCON512_CLEAN_FFT(f_gold, FALCON_LOGN);
+    // PQCLEAN_FALCON512_CLEAN_FFT(f_gold, FALCON_LOGN);
     // PQCLEAN_FALCON512_CLEAN_FFT_original(f_test, FALCON_LOGN);
     // print_layer(f_gold, 16, FALCON_N);
-    print_array(f_gold, FALCON_N, "FFT", 1);
+    // print_array(f_gold, FALCON_N, "FFT", 1);
     
     // print_array(f_gold, FALCON_N, "Before iFFT", 1);
     // PQCLEAN_FALCON512_CLEAN_iFFT_original(f_test, FALCON_LOGN);
-    // print_array(f_gold, FALCON_N, "iFFT", 1);
-    // ret |= compare(f_gold, f_test, FALCON_N, "Compare with original");
+    PQCLEAN_FALCON512_CLEAN_poly_split_fft(f0_gold, f1_gold, f_gold, size);
+    // print_array(f_gold,  1 << (size  ), "f_gold", 1);
+    // print_array(f0_gold, 1 << (size-1), "f0_gold", 1);
+    // print_array(f1_gold, 1 << (size-1), "f1_gold", 1);
+    PQCLEAN_FALCON512_CLEAN_poly_merge_fft(f_test, f0_gold, f1_gold, size);
+    PQCLEAN_FALCON512_CLEAN_poly_merge_fft_original(f_gold, f0_gold, f1_gold, size);
+    // print_array(f_test,  1 << (size  ), "f_test", 1);
+
+    ret |= compare(f_gold, f_test, 1 << size, "Compare with original");
 
     return ret;
 }
