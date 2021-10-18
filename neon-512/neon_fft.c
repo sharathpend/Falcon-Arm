f06175c02b385c96aed17765797834ab27d22b02
cothan <cothannguyen@gmail.com>
2021-10-18 06:49:38 -0400

    update

--- neon-512/neon_fft.c
+++ neon-512/neon_fft.c
@@ -5,19 +5,18 @@
 
 int main()
 {
-    fpr f_ifft[FALCON_N], f_fft[FALCON_N];
+    fpr f_gold[FALCON_N], f_test[FALCON_N];
     fpr f0_gold[FALCON_N], f1_gold[FALCON_N];
     fpr f0_test[FALCON_N], f1_test[FALCON_N];
-    fpr f_test[FALCON_N];
     fpr tmp;
-    int size = 7;
+    int size = 8;
     int ret = 0;
 
     for (int i = 0; i < FALCON_N; i++)
     {
         tmp = i;
-        f_ifft[i] = tmp;
-        f_fft[i] = tmp;
+        f_gold[i] = tmp;
+        f_test[i] = tmp;
         f0_gold[i] = tmp;
         f1_gold[i] = tmp;
     }
@@ -26,11 +25,15 @@ int main()
     // PQCLEAN_FALCON512_NEON_FFT(f_fft);
     // print_layer(f_fft, 16, FALCON_N);
     // print_array(f_fft, FALCON_N, "FFT", 1);
-
+    
+    PQCLEAN_FALCON512_NEON_poly_split_fft(f0_gold, f1_gold, f_gold, size);
     PQCLEAN_FALCON512_NEON_poly_merge_fft(f_test, f0_gold, f1_gold, size);
+
+    print_array(f0_gold, 1 << (size - 1), "f0_gold", 1);
+    print_array(f1_gold, 1 << (size - 1), "f1_gold", 1);
     print_array(f_test, 1 << size, "f_test", 1);
 
-    // print_array(f_ifft, FALCON_N, "Before iFFT", 1);
+    ret |= compare(f_gold, f_test, 1 << size, "Compare with original");
 
     return 0;
 }
