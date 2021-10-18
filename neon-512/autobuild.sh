398b51a92edf65981cbad277ef474a58609de877
cothan <cothannguyen@gmail.com>
2021-10-18 06:49:51 -0400

    update

--- neon-512/autobuild.sh
+++ neon-512/autobuild.sh
@@ -5,8 +5,8 @@
 # ./neon-fft | md5sum - 
 
 set -e 
-while inotifywait -e close_write fft.c neon_fft.c; do 
-    gcc -o neon-fft fips202.c fpr.c fft_split_merge.c inner.c util.c fft.c neon_fft.c -O3 -g3 -Wall
+while inotifywait -e close_write fft.c neon_fft.c fft_split_merge.c; do 
+    gcc -o neon-fft fips202.c fpr.c fpr_half.c fft_split_merge.c inner.c util.c fft.c neon_fft.c -O3 -g3 -Wall
     ./neon-fft
     ./neon-fft | md5sum - 
 done
