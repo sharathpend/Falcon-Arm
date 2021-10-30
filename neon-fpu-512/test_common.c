33ea95c39367ef8b475be0ba93c03b06ef6ff107
cothan <cothannguyen@gmail.com>
2021-10-30 00:49:11 -0400

    todo: test

--- neon-fpu-512/test_common.c
+++ neon-fpu-512/test_common.c
@@ -47,7 +47,7 @@ int
         ng |= s;
     }
     s |= -(ng >> 31);
-
+    printf("ref  ng-s: %u-%u\n", ng, s);
     return s <= l2bound[logn];
 }
 
@@ -73,8 +73,8 @@ int
     ngx4 = vld4q_dup_s32(0);
     for (size_t u = 0; u < n; u += 32)
     {
-        z1 = vld1q_s16_x4(&s1[u]);
-        z2 = vld1q_s16_x4(&s2[u]);
+        z1 = vld1q_u16_x4(&s1[u]);
+        z2 = vld1q_u16_x4(&s2[u]);
 
         vmulla_lo(s_lo.val[0], s_lo.val[0], z1.val[0], z1.val[0]);
         vmulla_hi(s_hi.val[0], s_hi.val[0], z1.val[0], z1.val[0]);
@@ -116,6 +116,7 @@ int
         vor(ngx4.val[3], ngx4.val[3], s_lo.val[3]);
         vor(ngx4.val[3], ngx4.val[3], s_hi.val[3]);
     }
+    // Collapse s_lo, s_hi
     vadd(s_lo.val[0], s_lo.val[0], s_hi.val[0]);
     vadd(s_lo.val[1], s_lo.val[1], s_hi.val[1]);
     vadd(s_lo.val[2], s_lo.val[2], s_hi.val[2]);
@@ -126,24 +127,35 @@ int
     vor(ngx4.val[2], ngx4.val[2], s_lo.val[2]);
     vor(ngx4.val[3], ngx4.val[3], s_lo.val[3]);
     
+    // Collapse s_lo 4 down to 2
     vadd(s_lo.val[0], s_lo.val[0], s_lo.val[2]);
     vadd(s_lo.val[1], s_lo.val[1], s_lo.val[3]);
-    vor(ngx4.val[0], ngx4.val[0], ngx4.val[2]);
-    vor(ngx4.val[1], ngx4.val[1], ngx4.val[3]);
-    
     vor(ngx4.val[0], ngx4.val[0], s_lo.val[0]);
     vor(ngx4.val[1], ngx4.val[1], s_lo.val[1]);
+
+    // Collapse ngx4 4 downto 2
+    vor(ngx4.val[0], ngx4.val[0], ngx4.val[2]);
+    vor(ngx4.val[1], ngx4.val[1], ngx4.val[3]);
+    // Collapse ngx4 2 downto 1
     vor(ngx4.val[0], ngx4.val[0], ngx4.val[1]);
-    
+    // Collapse s_lo 2 downto 1
     vadd(s_lo.val[0], s_lo.val[0], s_lo.val[1]);
     vor(ngx4.val[0], ngx4.val[0], s_lo.val[0]);
     
-    vpadd_u32(vget_low_u32(s_lo.val[0]), vget_high_u32(s_lo.val[0]));
-
-    vaddv(s, s_lo.val[0]);
-    
-    ng |= s;
+    uint32x2_t tmp, sx2, ngx2;
+    // Collapse s_lox4 down to x2
+    sx2 = vadd_u32(vget_low_u32(s_lo.val[0]), vget_high_u32(s_lo.val[0]));
+    // Collapse ngx4 downto x2
+    ngx2 = vorr_s32(vget_low_s32(ngx4.val[0]), vget_high_s32(ngx4.val[1]));
+    ngx2 = vorr_s32(ngx2, sx2);
+    // Collapse s_lox2 downto x1
+    s = vaddv_s32(sx2);
+    // No instruction to collapse ngx2 to ngx1, so move to general purpose register
+    ng |= vget_lane_u32(ngx2, 0);
+    ng |= vget_lane_u32(ngx2, 1);
+    ng |= s; 
     s |= -(ng >> 31);
+    printf("neon ng-s: %u-%u\n", ng, s);
 
     return s <= l2bound[logn];
 }
