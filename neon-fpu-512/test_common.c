f12b9f03da40e5d1e83c7d2183b0f9ee2fad95fd
cothan <cothannguyen@gmail.com>
2021-10-30 02:47:16 -0400

    finish

--- neon-fpu-512/test_common.c
+++ neon-fpu-512/test_common.c
@@ -3,6 +3,8 @@
 #include "macro.h"
 #include <stdio.h>
 
+#define DEBUG 0
+
 static const uint32_t l2bound[] = {
     0, /* unused */
     101498,
@@ -44,10 +46,11 @@ int
         s += (uint32_t)(z * z);
         ng |= s;
     }
-    s |= -(ng >> 31);
 #if DEBUG
-    printf("ref  ng-s: %u - %u\n", ng, s);
+    printf("is_short ref  ng-s: %u - %u\n", ng, s);
+    return s;
 #endif
+    s |= -(ng >> 31);
     return s <= l2bound[logn];
 }
 
@@ -146,7 +149,7 @@ int
     // Collapse s_lox4 down to x2
     sx2 = vadd_u32(vget_low_u32(s_lo.val[0]), vget_high_u32(s_lo.val[0]));
     // Collapse ngx4 downto x2
-    ngx2 = vorr_u32(vget_low_u32(ngx4.val[0]), vget_high_u32(ngx4.val[1]));
+    ngx2 = vorr_u32(vget_low_u32(ngx4.val[0]), vget_high_u32(ngx4.val[0]));
     ngx2 = vorr_u32(ngx2, sx2);
     // Collapse s_lox2 downto x1
     s = vaddv_u32(sx2);
@@ -154,11 +157,39 @@ int
     ng |= vget_lane_u32(ngx2, 0);
     ng |= vget_lane_u32(ngx2, 1);
     ng |= s;
-    s |= -(ng >> 31);
 #if DEBUG
-    printf("neon ng-s: %u - %u\n", ng, s);
+    printf("is_short neon ng-s: %u - %u\n", ng, s);
+    return s;
 #endif
+    s |= -(ng >> 31);
     return s <= l2bound[logn];
+}
+
+/* see inner.h */
+int
+    Zf(is_short_half)(
+        uint32_t sqn, const int16_t *s2, const unsigned logn)
+{
+    size_t n, u;
+    uint32_t ng;
+
+    n = (size_t)1 << logn;
+	ng = -(sqn >> 31);
+	for (u = 0; u < n; u ++) {
+		int32_t z;
+
+		z = s2[u];
+		sqn += (uint32_t)(z * z);
+		ng |= sqn;
+	}
+    
+#if DEBUG
+    printf("is_short_half ref  ng-sqn: %u - %u\n", ng, sqn);
+    return sqn;
+#endif
+    sqn |= -(ng >> 31);
+
+	return sqn <= l2bound[logn];
 }
 
 /* see inner.h */
@@ -166,22 +197,88 @@ int
     Zf(neon_is_short_half)(
         uint32_t sqn, const int16_t *s2, const unsigned logn)
 {
-    size_t n, u;
+    size_t n;
     uint32_t ng;
 
     n = (size_t)1 << logn;
     ng = -(sqn >> 31);
-    for (u = 0; u < n; u++)
+
+    int16x8x4_t z2;
+    uint32x4x4_t s_lo, s_hi, ngx4;
+    vdup32x4(s_lo, 0);
+    s_lo.val[0] = vsetq_lane_u32(sqn, s_lo.val[0], 0);
+    vdup32x4(s_hi, 0);
+    vdup32x4(ngx4, 0);
+    for (size_t u = 0; u < n; u += 32)
     {
-        int32_t z;
+        z2 = vld1q_s16_x4(&s2[u]);
 
-        z = s2[u];
-        sqn += (uint32_t)(z * z);
-        ng |= sqn;
+        vmulla_lo(s_lo.val[0], s_lo.val[0], z2.val[0], z2.val[0]);
+        vmulla_hi(s_hi.val[0], s_hi.val[0], z2.val[0], z2.val[0]);
+        vor(ngx4.val[0], ngx4.val[0], s_lo.val[0]);
+        vor(ngx4.val[0], ngx4.val[0], s_hi.val[0]);
+
+        vmulla_lo(s_lo.val[1], s_lo.val[1], z2.val[1], z2.val[1]);
+        vmulla_hi(s_hi.val[1], s_hi.val[1], z2.val[1], z2.val[1]);
+        vor(ngx4.val[1], ngx4.val[1], s_lo.val[1]);
+        vor(ngx4.val[1], ngx4.val[1], s_hi.val[1]);
+
+        vmulla_lo(s_lo.val[2], s_lo.val[2], z2.val[2], z2.val[2]);
+        vmulla_hi(s_hi.val[2], s_hi.val[2], z2.val[2], z2.val[2]);
+        vor(ngx4.val[2], ngx4.val[2], s_lo.val[2]);
+        vor(ngx4.val[2], ngx4.val[2], s_hi.val[2]);
+
+        vmulla_lo(s_lo.val[3], s_lo.val[3], z2.val[3], z2.val[3]);
+        vmulla_hi(s_hi.val[3], s_hi.val[3], z2.val[3], z2.val[3]);
+        vor(ngx4.val[3], ngx4.val[3], s_lo.val[3]);
+        vor(ngx4.val[3], ngx4.val[3], s_hi.val[3]);
     }
-    sqn |= -(ng >> 31);
+    vadd(s_lo.val[0], s_lo.val[0], s_hi.val[0]);
+    vadd(s_lo.val[1], s_lo.val[1], s_hi.val[1]);
+    vadd(s_lo.val[2], s_lo.val[2], s_hi.val[2]);
+    vadd(s_lo.val[3], s_lo.val[3], s_hi.val[3]);
 
-    return sqn <= l2bound[logn];
+    vor(ngx4.val[0], ngx4.val[0], s_lo.val[0]);
+    vor(ngx4.val[1], ngx4.val[1], s_lo.val[1]);
+    vor(ngx4.val[2], ngx4.val[2], s_lo.val[2]);
+    vor(ngx4.val[3], ngx4.val[3], s_lo.val[3]);
+
+    // Collapse s_lo 4 down to 2
+    vadd(s_lo.val[0], s_lo.val[0], s_lo.val[2]);
+    vadd(s_lo.val[1], s_lo.val[1], s_lo.val[3]);
+    vor(ngx4.val[0], ngx4.val[0], s_lo.val[0]);
+    vor(ngx4.val[1], ngx4.val[1], s_lo.val[1]);
+
+    // Collapse ngx4 4 downto 2
+    vor(ngx4.val[0], ngx4.val[0], ngx4.val[2]);
+    vor(ngx4.val[1], ngx4.val[1], ngx4.val[3]);
+    // Collapse ngx4 2 downto 1
+    vor(ngx4.val[0], ngx4.val[0], ngx4.val[1]);
+    // Collapse s_lo 2 downto 1
+    vadd(s_lo.val[0], s_lo.val[0], s_lo.val[1]);
+    vor(ngx4.val[0], ngx4.val[0], s_lo.val[0]);
+
+    uint32x2_t tmp, sx2, ngx2;
+    uint32_t s;
+    // Collapse s_lox4 down to x2
+    sx2 = vadd_u32(vget_low_u32(s_lo.val[0]), vget_high_u32(s_lo.val[0]));
+    // Collapse ngx4 downto x2
+    ngx2 = vorr_u32(vget_low_u32(ngx4.val[0]), vget_high_u32(ngx4.val[0]));
+    ngx2 = vorr_u32(ngx2, sx2);
+    // Collapse s_lox2 downto x1
+    s = vaddv_u32(sx2);
+    // No instruction to collapse ngx2 to ngx1, so move to general purpose register
+    ng |= vget_lane_u32(ngx2, 0);
+    ng |= vget_lane_u32(ngx2, 1);
+    ng |= s;
+    
+#if DEBUG
+    printf("is_short_half neon ng-sqn: %u - %u\n", ng, s);
+    return s;
+#endif
+
+    s |= -(ng >> 31);
+    return s <= l2bound[logn];
 }
 
 #define TESTS 100000
@@ -191,6 +288,7 @@ int main()
 {
     int16_t s1[1 << LOGN], s2[1 << LOGN];
     int16_t tmp, tmp2;
+    uint32_t sqn;
     int test, gold, ret = 0;
     for (int i = 0; i < TESTS; i++)
     {
@@ -198,17 +296,37 @@ int main()
         // Start TESTS
         for (int j = 0; j < (1 << LOGN); j++)
         {
+#if DEBUG
+            tmp = j;
+            tmp2 = rand();
+            s1[j] = tmp;
+            s2[j] = tmp + 123;
+#else 
             tmp = rand();
             tmp2 = rand();
             s1[j] = tmp;
             s2[j] = tmp2;
+#endif 
         }
         gold = falcon_inner_is_short(s1, s2, LOGN);
         test = falcon_inner_neon_is_short(s1, s2, LOGN);
 
         if (gold != test)
         {
-            printf("Error: %d != %d\n", gold, test);
+            printf("is_short Error: %d != %d\n", gold, test);
+            ret |= 1;
+        }
+#if DEBUG
+        sqn = 0;
+#else
+        sqn = rand();
+#endif
+        gold = falcon_inner_is_short_half(sqn, s2, LOGN);
+        test = falcon_inner_neon_is_short_half(sqn, s2, LOGN);
+
+        if (gold != test)
+        {
+            printf("is_short_half Error: %d != %d\n", gold, test);
             ret |= 1;
         }
 
