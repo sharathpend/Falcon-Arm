0cfd58ae24c841761e4631efefc0bba827743ce4
cothan <cothannguyen@gmail.com>
2021-11-01 00:39:58 -0400

    vectorize sampler

--- neon-fpu-512/sign_sampler.c
+++ neon-fpu-512/sign_sampler.c
@@ -74,24 +74,73 @@ Zf(gaussian0_sampler)(prng *p)
 	v1 = (uint32_t)(lo >> 24) & 0xFFFFFF;
 	v2 = (uint32_t)(lo >> 48) | (hi << 16);
 
-	/*
-	 * Sampled value is z, such that v0..v2 is lower than the first
-	 * z elements of the table.
-	 */
-	z = 0;
-	for (u = 0; u < (sizeof dist) / sizeof(dist[0]); u += 3) {
-		uint32_t w0, w1, w2, cc;
+    /*
+     * Sampled value is z, such that v0..v2 is lower than the first
+     * z elements of the table.
+     */
 
-		w0 = dist[u + 2];
-		w1 = dist[u + 1];
-		w2 = dist[u + 0];
-		cc = (v0 - w0) >> 31;
-		cc = (v1 - w1 - cc) >> 31;
-		cc = (v2 - w2 - cc) >> 31;
-		z += (int)cc;
-	}
-	return z;
+    uint32x4x3_t w;
+    uint32x4_t x0, x1, x2, cc0, cc1, cc2, zz;
+    uint32x2x3_t wh;
+    uint32x2_t cc0h, cc1h, cc2h, zzh;
+    x0 = vdupq_n_u32(v0);
+    x1 = vdupq_n_u32(v1);
+    x2 = vdupq_n_u32(v2);
 
+    // 0: 0, 3, 6, 9
+    // 1: 1, 4, 7, 10
+    // 2: 2, 5, 8, 11
+    // v0 - w0
+    // v1 - w1
+    // v2 - w2
+    // cc1 - cc0 >> 31
+    // cc2 - cc1 >> 31
+    // z + cc2 >> 31
+    w = vld3q_u32(&dist[0]);
+    cc0 = vsubq_u32(x0, w.val[2]);
+    cc1 = vsubq_u32(x1, w.val[1]);
+    cc2 = vsubq_u32(x2, w.val[0]);
+    cc1 = (uint32x4_t)vsraq_n_s32((int32x4_t)cc1, (int32x4_t)cc0, 31);
+    cc2 = (uint32x4_t)vsraq_n_s32((int32x4_t)cc2, (int32x4_t)cc1, 31);
+    zz = vshrq_n_u32(cc2, 31);
+
+    w = vld3q_u32(&dist[12]);
+    cc0 = vsubq_u32(x0, w.val[2]);
+    cc1 = vsubq_u32(x1, w.val[1]);
+    cc2 = vsubq_u32(x2, w.val[0]);
+    cc1 = (uint32x4_t)vsraq_n_s32((int32x4_t)cc1, (int32x4_t)cc0, 31);
+    cc2 = (uint32x4_t)vsraq_n_s32((int32x4_t)cc2, (int32x4_t)cc1, 31);
+    zz = vsraq_n_u32(zz, cc2, 31);
+
+    w = vld3q_u32(&dist[24]);
+    cc0 = vsubq_u32(x0, w.val[2]);
+    cc1 = vsubq_u32(x1, w.val[1]);
+    cc2 = vsubq_u32(x2, w.val[0]);
+    cc1 = (uint32x4_t)vsraq_n_s32((int32x4_t)cc1, (int32x4_t)cc0, 31);
+    cc2 = (uint32x4_t)vsraq_n_s32((int32x4_t)cc2, (int32x4_t)cc1, 31);
+    zz = vsraq_n_u32(zz, cc2, 31);
+
+    w = vld3q_u32(&dist[36]);
+    cc0 = vsubq_u32(x0, w.val[2]);
+    cc1 = vsubq_u32(x1, w.val[1]);
+    cc2 = vsubq_u32(x2, w.val[0]);
+    cc1 = (uint32x4_t)vsraq_n_s32((int32x4_t)cc1, (int32x4_t)cc0, 31);
+    cc2 = (uint32x4_t)vsraq_n_s32((int32x4_t)cc2, (int32x4_t)cc1, 31);
+    zz = vsraq_n_u32(zz, cc2, 31);
+
+    // 0: 48, 51
+    // 1: 49, 52
+    // 2: 50, 53
+    wh = vld3_u32(&dist[48]);
+    cc0h = vsub_u32(vget_low_u32(x0), wh.val[2]);
+    cc1h = vsub_u32(vget_low_u32(x1), wh.val[1]);
+    cc2h = vsub_u32(vget_low_u32(x2), wh.val[0]);
+    cc1h = (uint32x2_t)vsra_n_s32((int32x2_t)cc1h, (int32x2_t)cc0h, 31);
+    cc2h = (uint32x2_t)vsra_n_s32((int32x2_t)cc2h, (int32x2_t)cc1h, 31);
+    zzh = vshr_n_u32(cc2h, 31);
+
+    z = vaddvq_u32(zz) + vaddv_u32(zzh);
+    return z;
 }
 
 /*
