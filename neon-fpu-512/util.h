5b7f55bbe1455e76196fd806f4be3ef5fdda85ac
cothan <cothannguyen@gmail.com>
2021-11-02 12:29:42 -0400

    move share function to util.c

---
+++ neon-fpu-512/util.h
@@ -0,0 +1,10 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#define poly_small_to_fp(r, t, logn) smallints_to_fpr(r, t, logn)
+
+void smallints_to_fpr(fpr *r, const int8_t *t, unsigned logn);
+
+void print_farray(fpr *r, unsigned logn);
+
+#endif
