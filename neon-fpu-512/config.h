11ecc8b118cbdd30fff2456dbe3896c01ec81ece
cothan <cothannguyen@gmail.com>
2021-11-17 01:37:26 -0500

    add header

--- neon-fpu-512/config.h
+++ neon-fpu-512/config.h
@@ -1,9 +1,31 @@
+/*
+ * Fuse Multiply and Add configuration.
+ *
+ * =============================================================================
+ * Copyright (c) 2021 by Cryptographic Engineering Research Group (CERG)
+ * ECE Department, George Mason University
+ * Fairfax, VA, U.S.A.
+ * Author: Duc Tri Nguyen
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ * @author   Duc Tri Nguyen <dnguye69@gmu.edu>
+ */
+
+
 #ifndef CONFIG_H
 #define CONFIG_H
 
 /* 
  * Enable FMA/FMS instruction, this affect accuracy but better speed
  */
-#define FMA 0
+#define FMA 1
 
 #endif 
