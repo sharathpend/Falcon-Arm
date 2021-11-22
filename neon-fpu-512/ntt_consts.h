27038d7b32afca4588bcc5ce2d127f557f5b3b20
cothan <cothannguyen@gmail.com>
2021-11-22 03:05:36 -0500

    refactor ntt constants

---
+++ neon-fpu-512/ntt_consts.h
@@ -0,0 +1,24 @@
+#ifndef NTT_CONSTS
+#define NTT_CONSTS
+
+#include <stdint.h>
+#include "config.h"
+
+
+/*
+ * Table for NTT, binary case:
+ *   GMb[x] = R*(g^rev(x)) mod q
+ * where g = 7 (it is a 2048-th primitive root of 1 modulo q)
+ * and rev() is the bit-reversal function over 10 bits.
+ */
+extern const uint16_t ntt[];
+
+
+/*
+ * Table for inverse NTT, binary case:
+ *   iGMb[x] = R*((1/g)^rev(x)) mod q
+ * Since g = 7, 1/g = 8778 mod 12289.
+ */
+extern const uint16_t invntt[];
+
+#endif 
