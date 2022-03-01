efd9451eaf58786aa992c0423b2de591492d3693
cothan <cothanhahaha123@gmail.com>
2022-03-01 16:12:05 -0500

    clean up

--- neon-fpu-512/ntt_consts.c
+++ neon-fpu-512/ntt_consts.c
@@ -1,18 +1,15 @@
 #include "ntt_consts.h"
+#define PADDING 0
 
 #if FALCON_LOGN == 9
-// python gen_ntt.py 9 > ntt_consts9.c
+// ❯ python table_generate_512.py >>  ntt_consts9.c
 #include "ntt_consts9.c"
 
 #elif FALCON_LOGN == 10
-// python gen_ntt.py 10 > ntt_consts10.c
+// ❯ python table_generate_1024.py >>  ntt_consts10.c
 #include "ntt_consts10.c"
 
-#elif FALCON_LOGN == 8
-// python gen_ntt.py 8 > ntt_consts8.c
-#include "ntt_consts8.c"
-
 #else
-#error "Only support falcon_logn = 8,9,10"
+#error "Only support falcon_logn = 9,10"
 
 #endif
