e6b5af4568ba3ce37772f2d3ff0883d61b0a2a25
cothan <cothanhahaha123@gmail.com>
2022-03-02 21:48:30 -0500

    add barrett multiplication to butterfly units

--- neon-fpu-512/macrous.h
+++ neon-fpu-512/macrous.h
@@ -52,11 +52,20 @@
  * - Pointwise multiplication must use Montgomery *Doubling* arithmetic
  *
  * Rounding because:
+ *
  * - Montgomery need one coefficient to be *odd*, it only works with precomputed coefficient
- * - Barrett reduction work.
+ * => Tried this approach, very strict on coefficient input range.
+ * => E.g a*b: a in [-R/2, R/2]. b in [-Q/2, Q/2] then c in [-2Q, 2Q]
+ *
+ *  - Barrett multiplication seem to work better with no restriction
+ * => Proved to be good. E.g c=a*b, a in [-R, R], b in [-Q/2, Q/2] then c in [-3Q/2, 3Q/2]
+ *
+ * - Barrett reduction with c = a % Q. a in [-R, R] then c in [-Q/2, Q/2]
+ *
  *
  * Doubling because
  * - Montgomery Doubling work with two unknown coefficient, no constaint at all
+ * => c = a*b. a,b,c in [-R, R]
  */
 
 // ------------ Forward NTT and Inverse NTT ------------
@@ -80,55 +89,94 @@
     b = vqrdmlahq_s16(b, t, N);
 
 /*
+ * GS Butterfly with Barrett *Rounding* reduction
+ * Input: a in [-R, R], zl = w, zh = precomp_w, N, t
+ * Output: c = a * b % Q. c in [-3Q/2, 3Q/2]
+ */
+#define gsbf_br(a, b, zl, zh, Q, t) \
+    t = vsubq_s16(a, b);            \
+    a = vaddq_s16(a, b);            \
+    b = vmulq_s16(t, zh);           \
+    t = vqrdmulhq_s16(t, zl);       \
+    b = vmlsq_s16(b, t, Q);
+
+#define gsbf_bri(a, b, zl, zh, Q, t, i) \
+    t = vsubq_s16(a, b);                \
+    a = vaddq_s16(a, b);                \
+    b = vmulq_laneq_s16(t, zh, i);      \
+    t = vqrdmulhq_laneq_s16(t, zl, i);  \
+    b = vmlsq_s16(b, t, Q);
+
+/*
  * Montgomery multiplication via *Rounding* use only for Inverse NTT
- * Input: a, b, bNinv, N
+ * Input: a, b, bNinv, Q
  * Output: c = ab * R^-1
  */
-#define montmul_invntt(a, zl, zh, N, t, i) \
+#define montmul_invntt(a, zl, zh, Q, t, i) \
     a = vqrdmulhq_laneq_s16(a, zl, i);     \
     t = vmulq_laneq_s16(a, zh, i);         \
-    a = vqrdmlahq_s16(a, t, N);
+    a = vqrdmlahq_s16(a, t, Q);
 
 /*
  * CT Butterfly with Montgomery *Rounding* reduction
  * Input: a, b < R/2
  * Output: c in [-q, q], c = a * (2bR^-1)
  */
-#define ctbf_mt(a, b, zl, zh, N, t) \
+#define ctbf_mt(a, b, zl, zh, Q, t) \
     t = vqrdmulhq_s16(b, zl);       \
     b = vmulq_s16(b, zh);           \
-    t = vqrdmlahq_s16(t, b, N);     \
+    t = vqrdmlahq_s16(t, b, Q);     \
     b = vsubq_s16(a, t);            \
     a = vaddq_s16(a, t);
 
-#define ctbf_mti(a, b, zl, zh, N, t, i) \
+#define ctbf_mti(a, b, zl, zh, Q, t, i) \
     t = vqrdmulhq_laneq_s16(b, zl, i);  \
     b = vmulq_laneq_s16(b, zh, i);      \
-    t = vqrdmlahq_s16(t, b, N);         \
+    t = vqrdmlahq_s16(t, b, Q);         \
+    b = vsubq_s16(a, t);                \
+    a = vaddq_s16(a, t);
+
+/*
+ * CT Butterfly with Barrett *Rounding* reduction
+ * Input: a in [-R, R], zl = w, zh = precomp_w, N, t
+ * Output: c = a * b % Q. c in [-3Q/2, 3Q/2]
+ */
+#define ctbf_br(a, b, zl, zh, Q, t) \
+    t = vmulq_s16(b, zh);           \
+    b = vqrdmlahq_s16(b, zl);       \
+    t = vmlsq_s16(t, b, Q);         \
+    b = vsubq_s16(a, t);            \
+    a = vaddq_s16(a, t);
+
+#define ctbf_bri(a, b, zl, zh, Q, t, i) \
+    t = vmulq_s16(b, zh, i);            \
+    b = vqrdmlahq_s16(b, zl, i);        \
+    t = vmlsq_s16(t, b, Q);             \
     b = vsubq_s16(a, t);                \
     a = vaddq_s16(a, t);
 
 // ------------ Pointwise Multiplication ------------
 /*
  * Montgomery multiplication via *Doubling*
- * Input: a, b, bNinv, N
+ * Input: a, b, bNinv, Q
  * Output: c = ab * R^-1
  */
-#define montmul(c, a, b, t, neon_q, neon_qinv) \
-    c = vqdmulhq_s16(a, b);                    \
-    t = vmulq_s16(b, neon_qinv);               \
-    t = vmulq_s16(a, t);                       \
-    t = vqdmulhq_s16(t, neon_q);               \
+#define montmul(c, a, b, t, Q, Qinv) \
+    c = vqdmulhq_s16(a, b);          \
+    t = vmulq_s16(b, Qinv);          \
+    t = vmulq_s16(a, t);             \
+    t = vqdmulhq_s16(t, Q);          \
     c = vhsubq_u16(c, t);
 
 // ------------ Barrett Reduction ------------
-/* Barrett reduction, return [-Q/2, Q/2]
+/* 
+ * Barrett reduction, return [-Q/2, Q/2]
  * `v` = 5461, `n` = 11
  */
-#define barrett(a, t, neon_v, neon_q) \
-    t = vqdmulhq_s16(a, neon_v);      \
-    t = vrshrq_n_s16(t, 11);          \
-    a = vmlsq_s16(a, t, neon_q);
+#define barrett(a, t, V, Q)  \
+    t = vqdmulhq_s16(a, V);  \
+    t = vrshrq_n_s16(t, 11); \
+    a = vmlsq_s16(a, t, Q);
 
 // ------------ Matrix Transpose ------------
 /*
@@ -147,10 +195,10 @@
     v.val[3] = (int16x8_t)vtrn2q_s32((int32x4_t)tmp.val[1], (int32x4_t)tmp.val[3]);
 
 // ------------ Re-arrange vector ------------
-#define arrange(v_out, v_in, i, j, m, n)                                                  \
-    v_out.val[0] = (int16x8_t)vtrn1q_s64((int64x2_t)v_in.val[i], (int64x2_t)v_in.val[j]); \
-    v_out.val[2] = (int16x8_t)vtrn2q_s64((int64x2_t)v_in.val[i], (int64x2_t)v_in.val[j]); \
-    v_out.val[1] = (int16x8_t)vtrn1q_s64((int64x2_t)v_in.val[m], (int64x2_t)v_in.val[n]); \
-    v_out.val[3] = (int16x8_t)vtrn2q_s64((int64x2_t)v_in.val[m], (int64x2_t)v_in.val[n]);
+#define arrange(v_out, v_in, i, j, m, n, a, b, c, d)                                      \
+    v_out.val[a] = (int16x8_t)vtrn1q_s64((int64x2_t)v_in.val[i], (int64x2_t)v_in.val[j]); \
+    v_out.val[b] = (int16x8_t)vtrn2q_s64((int64x2_t)v_in.val[i], (int64x2_t)v_in.val[j]); \
+    v_out.val[c] = (int16x8_t)vtrn1q_s64((int64x2_t)v_in.val[m], (int64x2_t)v_in.val[n]); \
+    v_out.val[d] = (int16x8_t)vtrn2q_s64((int64x2_t)v_in.val[m], (int64x2_t)v_in.val[n]);
 
 #endif
