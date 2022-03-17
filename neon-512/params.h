a79d9dd56f6e07975c3d9b96a4a6274149cd4c0a
cothan <cothannguyen@gmail.com>
2022-03-17 15:53:50 -0400

    clean up

--- neon-512/params.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARAM_H
-#define PARAM_H
-
-#define FALCON_LOGN 10
-#define FALCON_N (1 << FALCON_LOGN)
-
-
-
-#endif
--- neon-512/sampler.h
+++
@@ -1,8 +0,0 @@
-#ifndef SAMPLER_H
-#define SAMPLER_H
-
-#include "fpr.h"
-
-int PQCLEAN_FALCON512_NEON_sampler(void *ctx, fpr mu, fpr isigma);
-
-#endif
