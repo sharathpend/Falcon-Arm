f878ea8b64537019ef2ce02fd76280774fecf4dc
cothan <cothannguyen@gmail.com>
2022-03-29 17:57:10 -0400

    use xtn instead of uzp to reserve sign bit

--- neon-fpu-512/common.c
+++ neon-fpu-512/common.c
@@ -34,21 +34,20 @@
 #include "macrofx4.h"
 
 /* see inner.h */
-void
-    Zf(hash_to_point_vartime)(
-        inner_shake256_context *sc,
-        uint16_t *x, unsigned logn)
+void Zf(hash_to_point_vartime)(
+    inner_shake256_context *sc,
+    uint16_t *x, unsigned logn)
 {
     /*
-	 * This is the straightforward per-the-spec implementation. It
-	 * is not constant-time, thus it might reveal information on the
-	 * plaintext (at least, enough to check the plaintext against a
-	 * list of potential plaintexts) in a scenario where the
-	 * attacker does not have access to the signature value or to
-	 * the public key, but knows the nonce (without knowledge of the
-	 * nonce, the hashed output cannot be matched against potential
-	 * plaintexts).
-	 */
+     * This is the straightforward per-the-spec implementation. It
+     * is not constant-time, thus it might reveal information on the
+     * plaintext (at least, enough to check the plaintext against a
+     * list of potential plaintexts) in a scenario where the
+     * attacker does not have access to the signature value or to
+     * the public key, but knows the nonce (without knowledge of the
+     * nonce, the hashed output cannot be matched against potential
+     * plaintexts).
+     */
     size_t n;
 
     n = (size_t)1 << logn;
@@ -59,7 +58,7 @@ void
 
         inner_shake256_extract(sc, (void *)buf, sizeof buf);
         w = ((unsigned)buf[0] << 8) | (unsigned)buf[1];
-        if (w < 5*FALCON_Q)
+        if (w < 5 * FALCON_Q)
         {
             while (w >= FALCON_Q)
             {
@@ -72,38 +71,37 @@ void
 }
 
 /* see inner.h */
-void
-    Zf(hash_to_point_ct)(
-        inner_shake256_context *sc,
-        uint16_t *x, unsigned logn, uint8_t *tmp)
+void Zf(hash_to_point_ct)(
+    inner_shake256_context *sc,
+    uint16_t *x, unsigned logn, uint8_t *tmp)
 {
     /*
-	 * Each 16-bit sample is a value in 0..65535. The value is
-	 * kept if it falls in 0..61444 (because 61445 = 5*12289)
-	 * and rejected otherwise; thus, each sample has probability
-	 * about 0.93758 of being selected.
-	 *
-	 * We want to oversample enough to be sure that we will
-	 * have enough values with probability at least 1 - 2^(-256).
-	 * Depending on degree N, this leads to the following
-	 * required oversampling:
-	 *
-	 *   logn     n  oversampling
-	 *     1      2     65
-	 *     2      4     67
-	 *     3      8     71
-	 *     4     16     77
-	 *     5     32     86
-	 *     6     64    100
-	 *     7    128    122
-	 *     8    256    154
-	 *     9    512    205
-	 *    10   1024    287
-	 *
-	 * If logn >= 7, then the provided temporary buffer is large
-	 * enough. Otherwise, we use a stack buffer of 63 entries
-	 * (i.e. 126 bytes) for the values that do not fit in tmp[].
-	 */
+     * Each 16-bit sample is a value in 0..65535. The value is
+     * kept if it falls in 0..61444 (because 61445 = 5*12289)
+     * and rejected otherwise; thus, each sample has probability
+     * about 0.93758 of being selected.
+     *
+     * We want to oversample enough to be sure that we will
+     * have enough values with probability at least 1 - 2^(-256).
+     * Depending on degree N, this leads to the following
+     * required oversampling:
+     *
+     *   logn     n  oversampling
+     *     1      2     65
+     *     2      4     67
+     *     3      8     71
+     *     4     16     77
+     *     5     32     86
+     *     6     64    100
+     *     7    128    122
+     *     8    256    154
+     *     9    512    205
+     *    10   1024    287
+     *
+     * If logn >= 7, then the provided temporary buffer is large
+     * enough. Otherwise, we use a stack buffer of 63 entries
+     * (i.e. 126 bytes) for the values that do not fit in tmp[].
+     */
 
     static const uint16_t overtab[] = {
         0, /* unused */
@@ -122,11 +120,11 @@ void
     uint16_t *tt1, tt2[63];
 
     /*
-	 * We first generate m 16-bit value. Values 0..n-1 go to x[].
-	 * Values n..2*n-1 go to tt1[]. Values 2*n and later go to tt2[].
-	 * We also reduce modulo q the values; rejected values are set
-	 * to 0xFFFF.
-	 */
+     * We first generate m 16-bit value. Values 0..n-1 go to x[].
+     * Values n..2*n-1 go to tt1[]. Values 2*n and later go to tt2[].
+     * We also reduce modulo q the values; rejected values are set
+     * to 0xFFFF.
+     */
     n = 1U << logn;
     n2 = n << 1;
     over = overtab[logn];
@@ -158,31 +156,31 @@ void
     }
 
     /*
-	 * Now we must "squeeze out" the invalid values. We do this in
-	 * a logarithmic sequence of passes; each pass computes where a
-	 * value should go, and moves it down by 'p' slots if necessary,
-	 * where 'p' uses an increasing powers-of-two scale. It can be
-	 * shown that in all cases where the loop decides that a value
-	 * has to be moved down by p slots, the destination slot is
-	 * "free" (i.e. contains an invalid value).
-	 */
+     * Now we must "squeeze out" the invalid values. We do this in
+     * a logarithmic sequence of passes; each pass computes where a
+     * value should go, and moves it down by 'p' slots if necessary,
+     * where 'p' uses an increasing powers-of-two scale. It can be
+     * shown that in all cases where the loop decides that a value
+     * has to be moved down by p slots, the destination slot is
+     * "free" (i.e. contains an invalid value).
+     */
     for (p = 1; p <= over; p <<= 1)
     {
         unsigned v;
 
         /*
-		 * In the loop below:
-		 *
-		 *   - v contains the index of the final destination of
-		 *     the value; it is recomputed dynamically based on
-		 *     whether values are valid or not.
-		 *
-		 *   - u is the index of the value we consider ("source");
-		 *     its address is s.
-		 *
-		 *   - The loop may swap the value with the one at index
-		 *     u-p. The address of the swap destination is d.
-		 */
+         * In the loop below:
+         *
+         *   - v contains the index of the final destination of
+         *     the value; it is recomputed dynamically based on
+         *     whether values are valid or not.
+         *
+         *   - u is the index of the value we consider ("source");
+         *     its address is s.
+         *
+         *   - The loop may swap the value with the one at index
+         *     u-p. The address of the swap destination is d.
+         */
         v = 0;
         for (u = 0; u < m; u++)
         {
@@ -204,32 +202,32 @@ void
             sv = *s;
 
             /*
-			 * The value in sv should ultimately go to
-			 * address v, i.e. jump back by u-v slots.
-			 */
+             * The value in sv should ultimately go to
+             * address v, i.e. jump back by u-v slots.
+             */
             j = u - v;
 
             /*
-			 * We increment v for the next iteration, but
-			 * only if the source value is valid. The mask
-			 * 'mk' is -1 if the value is valid, 0 otherwise,
-			 * so we _subtract_ mk.
-			 */
+             * We increment v for the next iteration, but
+             * only if the source value is valid. The mask
+             * 'mk' is -1 if the value is valid, 0 otherwise,
+             * so we _subtract_ mk.
+             */
             mk = (sv >> 15) - 1U;
             v -= mk;
 
             /*
-			 * In this loop we consider jumps by p slots; if
-			 * u < p then there is nothing more to do.
-			 */
+             * In this loop we consider jumps by p slots; if
+             * u < p then there is nothing more to do.
+             */
             if (u < p)
             {
                 continue;
             }
 
             /*
-			 * Destination for the swap: value at address u-p.
-			 */
+             * Destination for the swap: value at address u-p.
+             */
             if ((u - p) < n)
             {
                 d = &x[u - p];
@@ -245,9 +243,9 @@ void
             dv = *d;
 
             /*
-			 * The swap should be performed only if the source
-			 * is valid AND the jump j has its 'p' bit set.
-			 */
+             * The swap should be performed only if the source
+             * is valid AND the jump j has its 'p' bit set.
+             */
             mk &= -(((j & p) + 0x1FF) >> 9);
 
             *s = (uint16_t)(sv ^ (mk & (sv ^ dv)));
@@ -420,39 +418,37 @@ int ZfN(is_short_half)(uint32_t sqn, const int16_t *s2)
 
 void ZfN(sign_short_s1)(uint32_t *sqn_out, int16_t *s1tmp, const uint16_t *hm, const double *t0)
 {
-    float64x2x4_t neon_tf64[2];
-    int64x2x4_t neon_ts64[2];
-    int32x4x4_t neon_ts32, neon_hms32, z;
-    uint16x8x2_t neon_hm;
-    int16x8x2_t z16;
-    uint32x4_t neon_sqn, neon_ng;
-    uint16x8_t neon_zero;
+    // Total SIMD registers: 24 = 8 + 12
+    float64x2x4_t neon_tf640, neon_tf641; // 8
+    int64x2x4_t neon_ts640, neon_ts641;   // 8
+    int32x4x4_t neon_ts32, neon_hms32, z; // 12
+    uint16x8x2_t neon_hm;                 // 2
+    int16x8x2_t z16;                      // 2
+    uint32x4_t neon_sqn, neon_ng;         // 2
     uint32_t ng = 0, sqn = 0;
 
     neon_sqn = vdupq_n_u32(0);
     neon_ng = vdupq_n_u32(0);
-    neon_zero = vdupq_n_u16(0);
-    
 
     for (unsigned u = 0; u < FALCON_N; u += 16)
     {
 
-        vloadx4(neon_tf64[0], &t0[u]);
-        vloadx4(neon_tf64[1], &t0[u + 8]);
+        vloadx4(neon_tf640, &t0[u]);
+        vloadx4(neon_tf641, &t0[u + 8]);
         neon_hm = vld1q_u16_x2(&hm[u]);
 
-        vfrintx4(neon_ts64[0], neon_tf64[0]);
-        vfrintx4(neon_ts64[1], neon_tf64[1]);
+        vfrintx4(neon_ts640, neon_tf640);
+        vfrintx4(neon_ts641, neon_tf641);
 
-        neon_ts32.val[0] = vuzp1q_s32( (int32x4_t) neon_ts64[0].val[0], (int32x4_t) neon_ts64[0].val[1]);
-        neon_ts32.val[1] = vuzp1q_s32( (int32x4_t) neon_ts64[0].val[2], (int32x4_t) neon_ts64[0].val[3]);
-        neon_ts32.val[2] = vuzp1q_s32( (int32x4_t) neon_ts64[1].val[0], (int32x4_t) neon_ts64[1].val[1]);
-        neon_ts32.val[3] = vuzp1q_s32( (int32x4_t) neon_ts64[1].val[2], (int32x4_t) neon_ts64[1].val[3]);
+        neon_ts32.val[0] = vmovn_high_s64(vmovn_s64(neon_ts640.val[0]), neon_ts640.val[1]);
+        neon_ts32.val[1] = vmovn_high_s64(vmovn_s64(neon_ts640.val[2]), neon_ts640.val[3]);
+        neon_ts32.val[2] = vmovn_high_s64(vmovn_s64(neon_ts641.val[0]), neon_ts641.val[1]);
+        neon_ts32.val[3] = vmovn_high_s64(vmovn_s64(neon_ts641.val[2]), neon_ts641.val[3]);
 
-        neon_hms32.val[0] = (int32x4_t)vzip1q_u16( (uint16x8_t) neon_hm.val[0], (uint16x8_t) neon_zero);
-        neon_hms32.val[1] = (int32x4_t)vzip2q_u16( (uint16x8_t) neon_hm.val[0], (uint16x8_t) neon_zero);
-        neon_hms32.val[2] = (int32x4_t)vzip1q_u16( (uint16x8_t) neon_hm.val[1], (uint16x8_t) neon_zero);
-        neon_hms32.val[3] = (int32x4_t)vzip2q_u16( (uint16x8_t) neon_hm.val[1], (uint16x8_t) neon_zero);
+        neon_hms32.val[0] = vmovl_s16(vget_low_s16(neon_hm.val[0]));
+        neon_hms32.val[1] = vmovl_high_s16(neon_hm.val[0]);
+        neon_hms32.val[2] = vmovl_s16(vget_low_s16(neon_hm.val[1]));
+        neon_hms32.val[3] = vmovl_high_s16(neon_hm.val[1]);
 
         z.val[0] = vsubq_s32(neon_hms32.val[0], neon_ts32.val[0]);
         z.val[1] = vsubq_s32(neon_hms32.val[1], neon_ts32.val[1]);
@@ -460,19 +456,19 @@ void ZfN(sign_short_s1)(uint32_t *sqn_out, int16_t *s1tmp, const uint16_t *hm, c
         z.val[3] = vsubq_s32(neon_hms32.val[3], neon_ts32.val[3]);
 
         neon_sqn = vmlaq_u32(neon_sqn, (uint32x4_t)z.val[0], (uint32x4_t)z.val[0]);
-        neon_ng = vorrq_u32(neon_ng, neon_sqn);
+        vor(neon_ng, neon_ng, neon_sqn);
 
         neon_sqn = vmlaq_u32(neon_sqn, (uint32x4_t)z.val[1], (uint32x4_t)z.val[1]);
-        neon_ng = vorrq_u32(neon_ng, neon_sqn);
+        vor(neon_ng, neon_ng, neon_sqn);
 
         neon_sqn = vmlaq_u32(neon_sqn, (uint32x4_t)z.val[2], (uint32x4_t)z.val[2]);
-        neon_ng = vorrq_u32(neon_ng, neon_sqn);
+        vor(neon_ng, neon_ng, neon_sqn);
 
         neon_sqn = vmlaq_u32(neon_sqn, (uint32x4_t)z.val[3], (uint32x4_t)z.val[3]);
-        neon_ng = vorrq_u32(neon_ng, neon_sqn);
+        vor(neon_ng, neon_ng, neon_sqn);
 
-        z16.val[0] = vuzp1q_s16( (int16x8_t) z.val[0], (int16x8_t) z.val[1]);
-        z16.val[1] = vuzp1q_s16( (int16x8_t) z.val[2], (int16x8_t) z.val[3]);
+        z16.val[0] = vuzp1q_s16((int16x8_t)z.val[0], (int16x8_t)z.val[1]);
+        z16.val[1] = vuzp1q_s16((int16x8_t)z.val[2], (int16x8_t)z.val[3]);
 
         vst1q_s16_x2(&s1tmp[u], z16);
     }
@@ -498,36 +494,37 @@ void ZfN(sign_short_s1)(uint32_t *sqn_out, int16_t *s1tmp, const uint16_t *hm, c
 
 void ZfN(sign_short_s2)(int16_t *s2tmp, const double *t1)
 {
-    float64x2x4_t neon_tf64[4];
-    int64x2x4_t neon_ts64[4];
-    int32x4x4_t neon_ts32[2];
-    int16x8x4_t neon_s2;
+    // Total SIMD registers: 28 = 16 + 8 + 4
+    float64x2x4_t neon_tf640, neon_tf641, neon_tf642, neon_tf643; // 16
+    int64x2x4_t neon_ts640, neon_ts641, neon_ts642, neon_ts643;   // 16
+    int32x4x4_t neon_ts320, neon_ts321;                           // 8
+    int16x8x4_t neon_s2;                                          // 4
     for (unsigned u = 0; u < FALCON_N; u += 32)
     {
-        vloadx4(neon_tf64[0], &t1[u]);
-        vloadx4(neon_tf64[1], &t1[u + 8]);
-        vloadx4(neon_tf64[2], &t1[u + 16]);
-        vloadx4(neon_tf64[3], &t1[u + 24]);
+        vloadx4(neon_tf640, &t1[u]);
+        vloadx4(neon_tf641, &t1[u + 8]);
+        vloadx4(neon_tf642, &t1[u + 16]);
+        vloadx4(neon_tf643, &t1[u + 24]);
 
-        vfrintx4(neon_ts64[0], neon_tf64[0]);
-        vfrintx4(neon_ts64[1], neon_tf64[1]);
-        vfrintx4(neon_ts64[2], neon_tf64[2]);
-        vfrintx4(neon_ts64[3], neon_tf64[3]);
+        vfrintx4(neon_ts640, neon_tf640);
+        vfrintx4(neon_ts641, neon_tf641);
+        vfrintx4(neon_ts642, neon_tf642);
+        vfrintx4(neon_ts643, neon_tf643);
 
-        neon_ts32[0].val[0] = vuzp1q_s32((int32x4_t) neon_ts64[0].val[0], (int32x4_t) neon_ts64[0].val[1]);
-        neon_ts32[0].val[1] = vuzp1q_s32((int32x4_t) neon_ts64[0].val[2], (int32x4_t) neon_ts64[0].val[3]);
-        neon_ts32[0].val[2] = vuzp1q_s32((int32x4_t) neon_ts64[1].val[0], (int32x4_t) neon_ts64[1].val[1]);
-        neon_ts32[0].val[3] = vuzp1q_s32((int32x4_t) neon_ts64[1].val[2], (int32x4_t) neon_ts64[1].val[3]);
+        neon_ts320.val[0] = vmovn_high_s64(vmovn_s64(neon_ts640.val[0]), neon_ts640.val[1]);
+        neon_ts320.val[1] = vmovn_high_s64(vmovn_s64(neon_ts640.val[2]), neon_ts640.val[3]);
+        neon_ts320.val[2] = vmovn_high_s64(vmovn_s64(neon_ts641.val[0]), neon_ts641.val[1]);
+        neon_ts320.val[3] = vmovn_high_s64(vmovn_s64(neon_ts641.val[2]), neon_ts641.val[3]);
 
-        neon_ts32[1].val[0] = vuzp1q_s32((int32x4_t) neon_ts64[2].val[0], (int32x4_t) neon_ts64[2].val[1]);
-        neon_ts32[1].val[1] = vuzp1q_s32((int32x4_t) neon_ts64[2].val[2], (int32x4_t) neon_ts64[2].val[3]);
-        neon_ts32[1].val[2] = vuzp1q_s32((int32x4_t) neon_ts64[3].val[0], (int32x4_t) neon_ts64[3].val[1]);
-        neon_ts32[1].val[3] = vuzp1q_s32((int32x4_t) neon_ts64[3].val[2], (int32x4_t) neon_ts64[3].val[3]);
+        neon_ts321.val[0] = vmovn_high_s64(vmovn_s64(neon_ts642.val[0]), neon_ts642.val[1]);
+        neon_ts321.val[1] = vmovn_high_s64(vmovn_s64(neon_ts642.val[2]), neon_ts642.val[3]);
+        neon_ts321.val[2] = vmovn_high_s64(vmovn_s64(neon_ts643.val[0]), neon_ts643.val[1]);
+        neon_ts321.val[3] = vmovn_high_s64(vmovn_s64(neon_ts643.val[2]), neon_ts643.val[3]);
 
-        neon_s2.val[0] = vuzp1q_s16((int16x8_t) neon_ts32[0].val[0], (int16x8_t) neon_ts32[0].val[1]);
-        neon_s2.val[1] = vuzp1q_s16((int16x8_t) neon_ts32[0].val[2], (int16x8_t) neon_ts32[0].val[3]);
-        neon_s2.val[2] = vuzp1q_s16((int16x8_t) neon_ts32[1].val[0], (int16x8_t) neon_ts32[1].val[1]);
-        neon_s2.val[3] = vuzp1q_s16((int16x8_t) neon_ts32[1].val[2], (int16x8_t) neon_ts32[1].val[3]);
+        neon_s2.val[0] = vmovn_high_s32(vmovn_s32(neon_ts320.val[0]), neon_ts320.val[1]);
+        neon_s2.val[1] = vmovn_high_s32(vmovn_s32(neon_ts320.val[2]), neon_ts320.val[3]);
+        neon_s2.val[2] = vmovn_high_s32(vmovn_s32(neon_ts321.val[0]), neon_ts321.val[1]);
+        neon_s2.val[3] = vmovn_high_s32(vmovn_s32(neon_ts321.val[2]), neon_ts321.val[3]);
 
         neon_s2.val[0] = vnegq_s16(neon_s2.val[0]);
         neon_s2.val[1] = vnegq_s16(neon_s2.val[1]);
