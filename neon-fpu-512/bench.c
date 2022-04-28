7ec3a347ff06074ec1ee339ab5c37f48700c2f04
cothan <cothanhahaha123@gmail.com>
2022-04-28 02:20:50 -0400

    verbose benchmark

--- neon-fpu-512/bench.c
+++ neon-fpu-512/bench.c
@@ -60,8 +60,8 @@ void test_FFT(fpr *f, unsigned logn)
     /* =================================== */
     cc_ifft = CALC_CYCLES(start_cc, stop_cc, ntests);
     ns_ifft = CALC(start, stop, ntests);
-    printf("FFT (us) %u: %lld - %lld\n", logn, ns_fft, ns_ifft);
-    printf("FFT (cc) %u: %lld - %lld\n", logn, cc_fft, cc_ifft);
+    printf("FFT (us) %u: %lld - %lld | ", logn, ns_fft, ns_ifft);
+    printf("%lld - %lld\n", cc_fft, cc_ifft);
     printf("=======\n");
 }
 
@@ -84,7 +84,7 @@ void test_NTT(int16_t *a)
     TIME(stop);
     cc_fft = CALC_CYCLES(start_cc, stop_cc, NTESTS);
     ns_fft = CALC(start, stop, NTESTS);
-    
+
     /* =================================== */
     TIME(start);
     TIME_CYCLES(start_cc);
@@ -98,18 +98,192 @@ void test_NTT(int16_t *a)
     TIME(stop);
     cc_ifft = CALC_CYCLES(start_cc, stop_cc, NTESTS);
     ns_ifft = CALC(start, stop, NTESTS);
-    printf("NTT (us) %u: %lld - %lld\n", logn, ns_fft, ns_ifft);
-    printf("NTT (cc) %u: %lld - %lld\n", logn, cc_fft, cc_ifft);
+    printf("NTT (us) %u: %lld - %lld | ", logn, ns_fft, ns_ifft);
+    printf("%lld - %lld\n", cc_fft, cc_ifft);
+    printf("=======\n");
+}
+
+void test_poly_add(fpr *c, fpr *a, fpr *b, unsigned logn, char *string)
+{
+    long long start_cc, stop_cc;
+    long long cc_fft;
+    unsigned ntests = NTESTS;
+    if (logn < 10)
+    {
+        ntests = NTESTS * 1000;
+    }
+    /* =================================== */
+    TIME_CYCLES(start_cc);
+    for (unsigned i = 0; i < ntests; i++)
+    {
+        ZfN(poly_add)(c, a, b, logn);
+    }
+    TIME_CYCLES(stop_cc);
+    cc_fft = CALC_CYCLES(start_cc, stop_cc, ntests);
+
+    printf("%s, %u, %lld\n", string, logn, cc_fft);
+}
+
+void test_poly_sub(fpr *c, fpr *a, fpr *b, unsigned logn, char *string)
+{
+    long long start_cc, stop_cc;
+    long long cc_fft;
+    unsigned ntests = NTESTS;
+    if (logn < 10)
+    {
+        ntests = NTESTS * 1000;
+    }
+    /* =================================== */
+    TIME_CYCLES(start_cc);
+    for (unsigned i = 0; i < ntests; i++)
+    {
+        ZfN(poly_sub)(c, a, b, logn);
+    }
+    TIME_CYCLES(stop_cc);
+    cc_fft = CALC_CYCLES(start_cc, stop_cc, ntests);
+
+    printf("%s, %u, %lld\n", string, logn, cc_fft);
+}
+
+void test_poly_neg(fpr *c, fpr *a, fpr *b, unsigned logn, char *string)
+{
+    long long start_cc, stop_cc;
+    long long cc_fft;
+    unsigned ntests = NTESTS;
+    if (logn < 10)
+    {
+        ntests = NTESTS * 1000;
+    }
+    /* =================================== */
+    TIME_CYCLES(start_cc);
+    for (unsigned i = 0; i < ntests; i++)
+    {
+        ZfN(poly_neg)(c, a, logn);
+    }
+    TIME_CYCLES(stop_cc);
+    cc_fft = CALC_CYCLES(start_cc, stop_cc, ntests);
+
+    printf("%s, %u, %lld\n", string, logn, cc_fft);
+}
+
+void test_poly_adj_fft(fpr *c, fpr *a, fpr *b, unsigned logn, char *string)
+{
+    long long start_cc, stop_cc;
+    long long cc_fft;
+    unsigned ntests = NTESTS;
+    if (logn < 10)
+    {
+        ntests = NTESTS * 1000;
+    }
+    /* =================================== */
+    TIME_CYCLES(start_cc);
+    for (unsigned i = 0; i < ntests; i++)
+    {
+        ZfN(poly_adj_fft)(c, a, logn);
+    }
+    TIME_CYCLES(stop_cc);
+    cc_fft = CALC_CYCLES(start_cc, stop_cc, ntests);
+
+    printf("%s, %u, %lld\n", string, logn, cc_fft);
+}
+
+void test_poly_mul_fft(fpr *c, fpr *a, fpr *b, unsigned logn, char *string)
+{
+    long long start_cc, stop_cc;
+    long long cc_fft;
+    unsigned ntests = NTESTS;
+    if (logn < 7)
+    {
+        ntests = NTESTS * 100;
+    }
+    /* =================================== */
+    TIME_CYCLES(start_cc);
+    for (unsigned i = 0; i < ntests; i++)
+    {
+        ZfN(poly_mul_fft)(c, a, b, logn);
+    }
+    TIME_CYCLES(stop_cc);
+    cc_fft = CALC_CYCLES(start_cc, stop_cc, ntests);
+
+    printf("%s, %u, %lld\n", string, logn, cc_fft);
+}
+
+void test_poly_invnorm2_fft(fpr *c, fpr *a, fpr *b, unsigned logn, char *string)
+{
+    long long start_cc, stop_cc;
+    long long cc_fft;
+    unsigned ntests = NTESTS;
+    if (logn < 7)
+    {
+        ntests = NTESTS * 100;
+    }
+    /* =================================== */
+    TIME_CYCLES(start_cc);
+    for (unsigned i = 0; i < ntests; i++)
+    {
+        ZfN(poly_invnorm2_fft)(c, a, b, logn);
+    }
+    TIME_CYCLES(stop_cc);
+    cc_fft = CALC_CYCLES(start_cc, stop_cc, ntests);
+
+    printf("%s, %u, %lld\n", string, logn, cc_fft);
+}
+
+void test_poly_mul_autoadj_fft(fpr *c, fpr *a, fpr *b, unsigned logn, char *string)
+{
+    long long start_cc, stop_cc;
+    long long cc_fft;
+    unsigned ntests = NTESTS;
+    if (logn < 10)
+    {
+        ntests = NTESTS * 1000;
+    }
+    /* =================================== */
+    TIME_CYCLES(start_cc);
+    for (unsigned i = 0; i < ntests; i++)
+    {
+        ZfN(poly_mul_autoadj_fft)(c, a, b, logn);
+    }
+    TIME_CYCLES(stop_cc);
+    cc_fft = CALC_CYCLES(start_cc, stop_cc, ntests);
+
+    printf("%s, %u, %lld\n", string, logn, cc_fft);
+}
+
+void test_poly_LDL_fft(fpr *c, fpr *a, fpr *b, unsigned logn, char *string)
+{
+    long long start_cc, stop_cc;
+    long long cc_fft;
+    unsigned ntests = NTESTS;
+    if (logn < 10)
+    {
+        ntests = NTESTS * 1000;
+    }
+    /* =================================== */
+    TIME_CYCLES(start_cc);
+    for (unsigned i = 0; i < ntests; i++)
+    {
+        ZfN(poly_LDL_fft)(c, a, b, logn);
+    }
+    TIME_CYCLES(stop_cc);
+    cc_fft = CALC_CYCLES(start_cc, stop_cc, ntests);
+
+    printf("%s, %u, %lld\n", string, logn, cc_fft);
     printf("=======\n");
 }
 
 int main()
 {
-    fpr f[FALCON_N];
+    fpr f[FALCON_N], fa[FALCON_N], fb[FALCON_N], fc[FALCON_N];
     int16_t a[FALCON_N];
     for (int i = 0; i < FALCON_N; i++)
     {
-        f[i] = (double)i;
+        double_t t;
+        t = (double)i;
+        f[i] = t;
+        fa[i] = t;
+        fb[i] = t;
+        fc[i] = t;
         a[i] = rand() % FALCON_Q;
     }
 
@@ -121,8 +295,21 @@ int main()
     {
         test_FFT(f, i);
     }
-
     test_NTT(a);
+
+    for (unsigned i = 0; i <= FALCON_LOGN; i++)
+    {
+        test_poly_add(fc, fa, fb, i, "poly_add");
+        test_poly_sub(fc, fa, fb, i, "poly_sub");
+        test_poly_neg(fc, fa, fb, i, "poly_neg");
+        test_poly_adj_fft(fc, fa, fb, i, "poly_adj_fft");
+        test_poly_mul_fft(fc, fa, fb, i, "poly_mul_fft");
+        test_poly_invnorm2_fft(fc, fa, fb, i, "poly_invnorm2_fft");
+        test_poly_mul_autoadj_fft(fc, fa, fb, i, "poly_mul_autoadj_fft");
+        test_poly_LDL_fft(fc, fa, fb, i, "poly_LDL_fft");
+    }
+
+    
 
     return 0;
 }
